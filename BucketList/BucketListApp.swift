1ac4fae4012deb28dba2cfa35cf776b6775e4064
MaksimSamorokovskii <139316826+MaksimSamorokovskii@users.noreply.github.com>
2024-04-06 16:24:38 +0700

    added base files

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Максим Самороковский on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
